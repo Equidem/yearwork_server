2d802604bb8274da012568e90ec9acc3c0bf8543
ASUS <ASUS@ASUS-PC>
2012-12-18 15:16:15 +0100

    Upravil sem - Muffin 18.12.12 15:16

--- Onix/src/onix/MessageModul.java
+++ Onix/src/onix/MessageModul.java
@@ -1,24 +1,45 @@
 package onix;
 
 import com.google.gson.*;
+import java.util.Date;
 
 /**
- * Le já ~^_^~
- *  Muffin
- * Třída pro zpracování zpráv od klientů.
+ * Le já ~^_^~ Muffin Třída pro zpracování zpráv od klientů.
  */
-
 public class MessageModul {
+
+    static Gson GS;
     
-    public static void main(String[] args){
+    public MessageModul(Gson GS) {
+        this.GS = GS;
+    }
+
+    public static void main(String[] args, String m) {
+        Message mes = pcsMss(m);
+        sendMss(mes);
         
     }
-    
-    public static void pcsMss() { //process message - uložení zprávy do objektu.
-        
+
+    public static Message pcsMss(String m) { //process message - uložení zprávy do objektu.
+        Message mes;
+        String header = GS.fromJson(m, null);
+        String email = "";
+        String password = "";
+        int id = 0;
+        String nickname = "";
+        String status = "";
+        byte error = 0;
+        String sender = "";
+        String receiver = "";
+        Date dateTime = new Date(9999, 12, 31, 23, 59, 59);
+        String content = "";
+        mes = new Message(header, email, password, id, nickname, status, error, sender, receiver, dateTime, content);
+        if(id != 0) {
+            System.out.println("problem s id.");
+        }
+        return mes;
     }
-    
-    public static void sendMss() { //send message - odeslani pravy do web modulu nebo socket modulu
-        
+
+    public static void sendMss(Message mes) { //send message - odeslani pravy do web modulu nebo socket modulu
     }
 }
