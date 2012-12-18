968b88e2a09389e682c5559fc2d2f862f1bc8df7
ASUS <ASUS@ASUS-PC>
2012-12-18 13:25:44 +0100

    Upravil sem - Muffin

--- Onix/src/onix/Onix.java
+++ Onix/src/onix/Onix.java
@@ -14,7 +14,7 @@ public class Onix {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("pucík.");
         int x = 5;  
     }
 }
